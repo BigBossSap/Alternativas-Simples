167872628b14bfb4a407988fa92bedf0a3ede6e9
Sergi Alesanco Perich <sergi.alesanco.perich@gmail.com>
2022-10-30 17:49:51 +0100

    push

---
+++ Ex22/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Ex22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*22. Fes un programa que calculi la tarifa d’un pàrking, sabent que val 1€ la hora o fracció, és a
+dir que si estic 3 hores pago 3€, i si estic 3 hores i cinc minuts pago 4€. Per simplificar, per
+entrar les dades demana separadament l’hora d’entrada, el minut d’entrada, hora de
+sortida i minut de sortida. Suposem que tarifiquem dintre del mateix dia.*/
+
+            int hora, minutos, horaSalida, minutoSalida, horaTotal, minutosTotal, precioHora, precioFinal;
+
+
+            Console.WriteLine("Hora de entrada: ");
+            hora = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Minuto de entrada: ");
+            minutos = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            Console.WriteLine("Hora de salida: ");
+            horaSalida = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Minuto de salida: ");
+            minutoSalida = Convert.ToInt32(Console.ReadLine());
+
+
+
+            horaTotal = (int)((hora - horaSalida) * -1);
+            minutosTotal = (int)((minutos - minutoSalida) * -1);    
+            Console.WriteLine($"Tu tiempo total ha sido de {horaTotal} horas y {minutosTotal} minutos");
+
+            if (horaTotal >= 0 && horaTotal <= 24)
+            {
+                precioHora = horaTotal;
+
+                else if (minutosTotal >= 1)
+                    precioFinal = (int)precioHora + 1;
+                Console.WriteLine($"Precio final: {precioFinal}");
+
+            }
+
+
+                
+           
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Ex25/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ex25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 25. Fes un programa que donats tres números, els ordeni creixentment. Ha de permutar el valor
+de les variables si cal.
+*/
+
+
+            double num1, num2, num3;
+
+            Console.WriteLine("num1: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("num2: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("num3: ");
+            num3 = Convert.ToDouble(Console.ReadLine());
+
+            if (num1 >= num2 && num1 >= num3 && num2>= num3)
+                Console.WriteLine($"Orden creciente {num3} , {num2} , {num1}");
+            else if (num1 <= num2 && num1 <= num3 && num2 <= num3)
+                Console.WriteLine($"Orden creciente {num1} , {num2} , {num3}");
+            else 
+                Console.WriteLine($"Orden creciente {num3} , {num1} , {num2}");
+
+
+
+
+
+        }
+    }
+}
